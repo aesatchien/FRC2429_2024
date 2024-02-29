4674061db1eb2d47b3dceb60eeba40ca85a3e26d
Josh Sugino <jsugino25@gmail.com>
2024-02-28 19:56:49 -0800

    edited Leo's april tag code to ensure that camera is not detecting tag while turning (detect, then turn; not at the same time)

--- robot/commands/auto_turn.py
+++ robot/commands/auto_turn.py
@@ -2,21 +2,20 @@ import commands2
 from wpilib import SmartDashboard
 from wpimath.controller import PIDController
 from photonlibpy.photonCamera import PhotonCamera
-from commands.auto_turn import AutoTurn
 
 from subsystems.swerve import Swerve
 import constants
 
 
-class AutoAimAprilTag(commands2.Command):  # change the name for your command
+class AutoTurn(commands2.Command):  # change the name for your command
 
-    def __init__(self, container, drive: Swerve, target_tag_id: int) -> None:
+    def __init__(self, container, drive: Swerve, target_angle) -> None:
         super().__init__()
-        self.setName('Auto aim to AprilTag')  # change this to something appropriate for this command
+        self.setName('Auto Turn')  # change this to something appropriate for this command
         self.container = container
         self.drive = drive
-        self.target_tag_id = target_tag_id
-        self.camera = PhotonCamera(constants.k_camera_name) # not sure we need a whole vision subsystem just for 1 command
+        self.target_angle = target_angle
+        self.rot_PID_controller = PIDController(4, 0, 0) # Values are taken from Pathplanner's values in swerve.py
         self.addRequirements(drive)  # commandsv2 version of requirements
 
     def initialize(self) -> None:
@@ -26,21 +25,21 @@ class AutoAimAprilTag(commands2.Command):  # change the name for your command
         SmartDashboard.putString("alert",
                                  f"** Started {self.getName()} at {self.start_time - self.container.get_enabled_time():2.2f} s **")
 
-    def execute(self) -> None:
-        results = self.camera.getLatestResult()
-        targets = results.getTargets()
-        self.goodAprilTag = None
-        for detection in targets:
-            if detection.getFiducialId() == self.target_tag_id:
-                self.goodAprilTag = detection
-                break
+        self.rot_PID_controller.reset()
+        self.rot_PID_controller.setSetpoint(0) #sets robot's "0" to angle 0 (because we're turning relative to where the camera is looking at)
+        # self.rot_PID_controller.setTolerance(0.5) #sets the tolerance to 0.5 degrees
 
+    def execute(self) -> None:
+        desired_rotation = self.rot_PID_controller.calculate(self.target_angle, 0)
+        self.drive.drive(0, 0, desired_rotation, fieldRelative=False, rate_limited=True)
+        
     def isFinished(self) -> bool:
-        return self.goodAprilTag is not None
+        return self.rot_PID_controller.atSetpoint()
 
     def end(self, interrupted: bool) -> None:
-        AutoTurn(self.goodAprilTag.getYaw()).schedule()
+ 
         end_time = self.container.get_enabled_time()
+
         message = 'Interrupted' if interrupted else 'Ended'
         print(f"** {message} {self.getName()} at {end_time:.1f} s after {end_time - self.start_time:.1f} s **")
         SmartDashboard.putString(f"alert",
