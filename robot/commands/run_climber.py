f8fd02393104bd35747281771f20b0d62b15fbe1
Leo <leoslegos1@gmail.com>
2024-02-29 16:44:26 -0800

    turn climber toggle to while held

--- robot/commands/run_climber.py
+++ robot/commands/run_climber.py
@@ -3,14 +3,13 @@ import wpilib
 from wpilib import SmartDashboard
 from subsystems.climber import Climber
 
-class ClimberToggle(commands2.CommandBase):
+class RunClimber(commands2.CommandBase):
 
-    def __init__(self, container, climber:Climber, power=5, force=None, timeout=None) -> None:
+    def __init__(self, container, climber:Climber, power=5, timeout=None) -> None:
         super().__init__()
         self.setName('IndexerToggle')
         self.climber = climber
         self.container = container
-        self.force = force
         self.power = power
         self.timeout = timeout
         self.timer = wpilib.Timer()
@@ -21,24 +20,19 @@ class ClimberToggle(commands2.CommandBase):
         print("\n" + f"** Started {self.getName()} at {self.start_time} s **", flush=True)
 
         self.timer.restart()
-
-        if self.force == 'on':
-            self.climber.set_climber(self.power)
-        elif self.force == 'off':
-            self.climber.stop_climber()
-        else:
-            self.climber.toggle_climber(self.power)
+        self.climber.set_climber(self.power)
 
     def execute(self) -> None:
         pass
 
     def isFinished(self) -> bool:
         if self.timeout is None:
-            return True
+            return False
         else:
             return self.timer.get() > self.timeout
 
     def end(self, interrupted: bool) -> None:
+        self.climber.stop_climber()
         end_time = self.container.get_enabled_time()
         message = 'Interrupted' if interrupted else 'Ended'
         print(f"** {message} {self.getName()} at {end_time:.1f} s after {end_time - self.start_time:.1f} s **", flush=True)
