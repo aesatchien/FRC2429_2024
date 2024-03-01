84e8d70cb917be51d1a5dd726c7cbe51c103c597
aesatchien <cjh123@yaoo.com>
2024-03-01 14:20:05 -0800

    updates

--- robot/autonomous/aim_and_shoot.py
+++ robot/autonomous/aim_and_shoot.py
@@ -1,12 +1,11 @@
 import commands2
 import constants
 
-<<<<<<< HEAD
+
 from autonomous.auto_shoot_cycle import AutoShootCycle
 from commands.arm_cycle import ArmCycle
-=======
+
 from commands.arm_smart_go_to import ArmSmartGoTo
->>>>>>> a5c416d0e2c7be0d010fd296fb8698b1ea7f0a97
 from subsystems.lower_crank_trapezoid import LowerCrankArmTrapezoidal
 from subsystems.upper_crank_trapezoid import UpperCrankArmTrapezoidal
 from commands.arm_smart_go_to import ArmSmartGoTo
@@ -15,9 +14,5 @@ class AimAndShoot(commands2.SequentialCommandGroup):
         super().__init__()
         self.setName('AimAndShoot')  # change this to something appropriate for this command
         self.container = container
-<<<<<<< HEAD
         self.addCommands(ArmSmartGoTo(self.container, upper_crank=upper_arm, lower_crank=lower_arm, desired_position='shoot')) # Go to first position, specifically "shoot"
-=======
-        self.addCommands(ArmSmartGoTo(self.container, upper_arm, lower_arm, 'shoot')) # Go to first position, specifically "shoot"
->>>>>>> a5c416d0e2c7be0d010fd296fb8698b1ea7f0a97
         self.addCommands(AutoShootCycle(self.container))
