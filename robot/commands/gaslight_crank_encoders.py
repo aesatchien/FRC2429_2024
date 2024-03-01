e6de3b560efd3a89aab1f52824ae3f77d43d5bea
Leo <leoslegos1@gmail.com>
2024-03-01 13:51:06 -0800

    shooter back to 3000

--- robot/commands/gaslight_crank_encoders.py
+++ robot/commands/gaslight_crank_encoders.py
@@ -1,3 +1,4 @@
+import math
 import commands2
 from wpilib import SmartDashboard
 
@@ -11,7 +12,7 @@ class GaslightCrankEncoders(commands2.CommandBase):  # change the name for your 
         self.setName('Gaslight crank encoders')  # change this to something appropriate for this command
         self.container = container
         self.lower_crank = lower_crank
-        self.positions_to_gaslight_to = [constants.k_crank_arm_dict['min_angle'], constants.k_crank_presets['shoot']]
+        self.positions_to_gaslight_to = [constants.k_crank_arm_dict['min_angle'], constants.k_crank_presets['shoot']['lower']]
         self.counter = 0
         # self.addRequirements(self.container.)  # commandsv2 version of requirements
 
@@ -20,7 +21,10 @@ class GaslightCrankEncoders(commands2.CommandBase):  # change the name for your 
         # deactivate arm so it doesn't get confused as we gaslight it
         self.lower_crank.disable_arm()
         position_to_gaslight_to = self.positions_to_gaslight_to[self.counter % len(self.positions_to_gaslight_to)]
-        self.lower_crank.set_encoder_position(position_to_gaslight_to)
+        # We gaslight it to be somewhere, then tell it to go there; somehow it ungaslights itself
+        self.lower_crank.set_encoder_position(math.radians(position_to_gaslight_to))
+        self.lower_crank.set_goal(math.radians(position_to_gaslight_to))
+        self.counter += 1
         self.start_time = round(self.container.get_enabled_time(), 2)
         print("\n" + f"** Started {self.getName()} at {self.start_time} s gaslighting to {position_to_gaslight_to} **", flush=True)
         SmartDashboard.putString("alert",
