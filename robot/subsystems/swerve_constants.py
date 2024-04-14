fb76e3620656d16c552082859c00a2e4ccebfc8b
aesatchien <cjh123@yahoo.com>
2024-04-14 10:52:06 -0700

    increase rotation speed

--- robot/subsystems/swerve_constants.py
+++ robot/subsystems/swerve_constants.py
@@ -10,7 +10,7 @@ class DriveConstants:
     # the robot, rather the allowed maximum speeds
     k_drive_controller_type = CANSparkFlex
     kMaxSpeedMetersPerSecond = 4.75  # Sanjith started at 3.7, 4.25 was Haochen competition, 4.8 is full out on NEOs
-    kMaxAngularSpeed = 0.5 * math.tau  # radians per second
+    kMaxAngularSpeed = 0.75 * math.tau # 0.5 * math.tau  # radians per second was 0.5 tau through AVR - too slow
     # TODO: actually figure out what the total max speed should be - vector sum?
     kMaxTotalSpeed = 1.1 * math.sqrt(2) * kMaxSpeedMetersPerSecond  # sum of angular and rotational, should probably do hypotenuse
     # set the acceleration limits used in driving using the SlewRateLimiter tool
